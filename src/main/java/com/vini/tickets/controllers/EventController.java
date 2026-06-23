e9d49411baa273340d596edd3e4b41f586882d00
PlutoDog95 <agarwal.vineet2006@gmail.com>
2026-06-23 21:55:50 +0530

    Endpoint update

--- src/main/java/com/vini/tickets/controllers/EventController.java
+++ src/main/java/com/vini/tickets/controllers/EventController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+import static com.vini.tickets.util.JwtUtil.parseUserId;
+
 @RestController
 @RequestMapping("/api/v1/events")
 @RequiredArgsConstructor
@@ -95,9 +97,5 @@ public class EventController {
         UUID userId = parseUserId(jwt);
         eventService.deleteEventForOrganizer(userId, eventId);
         return ResponseEntity.noContent().build();
-    }
-
-    private UUID parseUserId(Jwt jwt) {
-        return UUID.fromString(jwt.getSubject());
     }
 }
